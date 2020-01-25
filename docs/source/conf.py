cef5827556560d48fe1a9d3d0930ee65ca50b48d
minosthem <minosthemelis@gmail.com>
2020-01-25 21:00:44 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'movie-recommendation-system'
+project = 'Filmoid'
 copyright = '2020, Minos Themelis'
 author = 'Minos Themelis'
 
