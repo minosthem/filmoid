82931ef386c30bf5ddc811571909ae14c5a9f465
minosthem <minosthemelis@gmail.com>
2020-01-25 21:14:50 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_favicon = 'favicon.ico'
+html_logo='filmoid.png'
